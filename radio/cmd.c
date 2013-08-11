f54b0a94d0a7dcefc664d06afbf23f75caa14c0f
Daniele Basile <asterix24@gmail.com>
2013-08-11 18:42:42 +0200

    Add commad module.

---
+++ radio/cmd.c
@@ -0,0 +1,65 @@
+/**
+ * \file
+ * <!--
+ * This file is part of BeRTOS.
+ *
+ * Bertos is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, you may use this file as part of a free software
+ * library without restriction.  Specifically, if other files instantiate
+ * templates or use macros or inline functions from this file, or you compile
+ * this file and link it with other files to produce an executable, this
+ * file does not by itself cause the resulting executable to be covered by
+ * the GNU General Public License.  This exception does not however
+ * invalidate any other reasons why the executable file might be covered by
+ * the GNU General Public License.
+ *
+ * Copyright 2013 Develer S.r.l. (http://www.develer.com/)
+ * All Rights Reserved.
+ * -->
+ *
+ * \brief Radio commands.
+ *
+ * \author Daniele Basile <asterix@develer.com>
+ */
+
+#include "protocol.h"
+
+#include <cfg/debug.h>
+
+#include <string.h>
+
+static int cmd_broadcast(KFile *fd, Protocol *proto)
+{
+	proto->data[proto->len] = '\0';
+	kprintf("type[%d], addr[%d], data[%s]\n", proto->type, proto->addr, proto->data);
+
+	uint8_t ack = PROTO_ACK;
+	int ret = protocol_reply(fd, proto, proto->addr, "ACK", sizeof("ACK"));
+	kprintf("Send[%d]\n", ret);
+
+	return 0;
+}
+
+const Cmd master_cmd[] = {
+	{ 0xFF, cmd_broadcast },
+	{ 0   , NULL }
+};
+
+const Cmd slave_cmd[] = {
+	{ 0     , NULL }
+};
+
+
