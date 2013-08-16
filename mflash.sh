535d22b694701f4513f52e8aac3ca221c4b469c6
Daniele Basile <asterix@develer.com>
2013-08-16 14:08:29 +0200

    Modify flash utils to can specify a serail port.

--- mflash.sh
+++ mflash.sh
@@ -1,1 +1,10 @@
-./stm32loader.py -p /dev/tty.usbserial-FTCVMTAV -w -e -v images/radio.bin
+#!/bin/bash
+
+PORT="/dev/tty.usbserial-FTCVMTAV"
+
+if [ $# -ge 1 ]; then
+	PORT=$1
+fi
+
+./stm32loader.py -p $PORT -w -e -v images/radio.bin
+
--- sflash.sh
+++ sflash.sh
@@ -1,1 +1,7 @@
-./stm32loader.py -p /dev/tty.usbserial -w -e -v images/radio.bin
+PORT="/dev/tty.usbserial"
+
+if [ $# -ge 1 ]; then
+	PORT=$1
+fi
+
+./stm32loader.py -p $PORT -w -e -v images/radio.bin
