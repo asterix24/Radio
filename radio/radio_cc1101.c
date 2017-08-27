07712b10fbd4b9f66d70f75ab743750a4e2789f0
Daniele Basile <asterix24@gmail.com>
2017-08-27 17:40:30 +0200

    Use alternate modem configuration.

--- radio/radio_cc1101.c
+++ radio/radio_cc1101.c
@@ -44,6 +44,42 @@
 
 #define CC1101_WAIT_IDLE_TIMEOUT   5000 // ms
 
+#if 1
+/*
+ * Datarate: 38.4 kBaud
+ * Freq: 868MHz
+ * GSFK, 100kHz
+ */
+const Setting ping_low_baud_868[] =
+{
+  {CC1101_IOCFG0,      0x01},
+  {CC1101_FIFOTHR,     0x47},
+  {CC1101_PKTCTRL0,    0x05},
+  {CC1101_FSCTRL1,     0x06},
+  {CC1101_FREQ2,       0x21},
+  {CC1101_FREQ1,       0x62},
+  {CC1101_FREQ0,       0x76},
+  {CC1101_MDMCFG4,     0xCA},
+  {CC1101_MDMCFG3,     0x83},
+  {CC1101_MDMCFG2,     0x13},
+  {CC1101_DEVIATN,     0x35},
+  {CC1101_MCSM0,       0x18},
+  {CC1101_FOCCFG,      0x16},
+  {CC1101_AGCCTRL2,    0x43},
+  {CC1101_WORCTRL,     0xFB},
+  {CC1101_FSCAL3,      0xE9},
+  {CC1101_FSCAL2,      0x2A},
+  {CC1101_FSCAL1,      0x00},
+  {CC1101_FSCAL0,      0x1F},
+  {CC1101_TEST2,       0x81},
+  {CC1101_TEST1,       0x35},
+  {CC1101_TEST0,       0x09},
+
+  { 0xff, 0xff },
+};
+
+#else
+
 const Setting ping_low_baud_868[] =
 {
   {CC1101_IOCFG0,      0x01 /* GDO0 Output Pin Configuration */},
@@ -85,6 +121,8 @@ const Setting ping_low_baud_868[] =
 
   { 0xff, 0xff },
 };
+
+#endif
 
 static bool wait_fifoAvail(mtime_t timeout)
 {
