3f8c51c25515279383c44ace0b8f99fe3d2dc74c
Daniele Basile <asterix24@gmail.com>
2017-09-19 21:18:41 +0200

    Clean up, revise code to be more simple.

--- radio/radio_cc1101.c
+++ radio/radio_cc1101.c
@@ -43,53 +43,15 @@
 #include <string.h>
 
 #define CC1101_WAIT_IDLE_TIMEOUT   5000 // ms
-
-#if 0
-/*
- * Datarate: 5 kBaud
- * Freq: 868MHz
- * GSFK, 23.5kHz
- */
-const Setting ping_low_baud_868[] =
-{
-  {CC1101_IOCFG0,      0x01},
-
-  {CC1101_FIFOTHR,     0x47},
-  {CC1101_PKTCTRL0,    0x05},
-  {CC1101_FSCTRL1,     0x06},
-  {CC1101_FREQ2,       0x21},
-  {CC1101_FREQ1,       0x62},
-  {CC1101_FREQ0,       0x76},
-  {CC1101_MDMCFG4,     0xC7},
-  {CC1101_MDMCFG3,     0x75},
-  {CC1101_MDMCFG2,     0x13},
-  {CC1101_DEVIATN,     0x37},
-  {CC1101_MCSM0,       0x18},
-  {CC1101_FOCCFG,      0x16},
-  {CC1101_AGCCTRL2,    0x43},
-  {CC1101_WORCTRL,     0xFB},
-  {CC1101_FSCAL3,      0xE9},
-  {CC1101_FSCAL2,      0x2A},
-  {CC1101_FSCAL1,      0x00},
-  {CC1101_FSCAL0,      0x1F},
-  {CC1101_TEST2,       0x81},
-  {CC1101_TEST1,       0x35},
-  {CC1101_TEST0,       0x09},
-
-  { 0xff, 0xff },
-};
-
-
-#else
+#define CC1101_GOIDLE_TIMEOUT      500  //ms
 
 const Setting ping_low_baud_868[] =
 {
-  {CC1101_IOCFG0,      0x06 /* GDO0 Output Pin Configuration */},
-  {CC1101_FIFOTHR,     0x47 /* RX FIFO and TX FIFO Thresholds */},
-  {CC1101_PKTCTRL0,    0x00 /* Packet Automation Control */},
-  {CC1101_PKTCTRL1,    0x00                 },
-  {CC1101_PKTLEN,      RADIO_MAXPAYLOAD_LEN },
-  {CC1101_CHANNR,      0x00 /* Channel Number */},
+  {CC1101_IOCFG0,      0x01 /* GDO0 Output Pin Configuration */},
+  {CC1101_FIFOTHR,     0x4F /* RX FIFO and TX FIFO Thresholds */},
+  {CC1101_PKTCTRL0,    0x05 /* Packet Automation Control */},
+  {CC1101_PKTCTRL1,    0x00 /* Packet Automation Control */},
+  {CC1101_CHANNR,      0x08 /* Channel Number */},
 
   {CC1101_FSCTRL1,     0x06 /* Frequency Synthesizer Control */},
   {CC1101_FREQ0,       0x76 /* Frequency Control Word, Low Byte */},
@@ -112,9 +74,8 @@ const Setting ping_low_baud_868[] =
 
   // {CC1101_PATABLE,     0xC0  /* Various Test Settings */ },
 
-  {CC1101_MDMCFG0,     0xC2 /* Modem Configuration */},
   {CC1101_MDMCFG1,     0x42 /* Modem Configuration */},
-#if 0//CONFIG_RADIO_CURRENT_OPT
+#if CONFIG_RADIO_CURRENT_OPT
   {CC1101_MDMCFG2,     0x93 /* Modem Configuration */},
 #else
   {CC1101_MDMCFG2,     0x13 /* Modem Configuration */},
@@ -125,8 +86,6 @@ const Setting ping_low_baud_868[] =
 
   { 0xff, 0xff },
 };
-
-#endif
 
 static bool wait_fifoAvail(mtime_t timeout)
 {
@@ -194,6 +153,7 @@ INLINE uint8_t radio_status(void)
 	return status;
 }
 
+
 void radio_sleep(void)
 {
 	LOG_INFO("Sleep: Rdy[%d] St[%d] FifoAvail[%d]\n", UNPACK_STATUS(cc1101_strobe(CC1101_SPWD)));
@@ -214,32 +174,19 @@ static void radio_clearerr(struct KFile *_fd)
 static size_t radio_send(struct KFile *_fd, const void *_buf, size_t size)
 {
 	/* The packet len is now fix to max 64 byte (see configuation)*/
-	ASSERT(size <= 64);
-	kprintf("TxLen[%d]\n", size);
-
 	const uint8_t *data = (const uint8_t *) _buf;
 	Radio *fd = RADIO_CAST(_fd);
 
-	if (!radio_waitIdle(1000))
-	{
-		fd->error = RADIO_TX_ERR;
-		return RADIO_TX_ERR;
-	}
-
-	// Flush Tx FiFo
+	radio_waitIdle(CC1101_GOIDLE_TIMEOUT);
 	cc1101_strobe(CC1101_SFTX);
-	// Go in Tx mode
 	cc1101_strobe(CC1101_STX);
 
 	fd->status = cc1101_write(CC1101_TXFIFO, data, size);
 	LOG_INFO("TxData[%d] s[%d]\n", size, STATUS_STATE(fd->status));
 
-	radio_waitIdle(1000);
-	// Flush Tx FiFo
+	radio_waitIdle(CC1101_GOIDLE_TIMEOUT);
 	LOG_INFO("Tx: s1[%d]\n", STATUS_STATE(cc1101_strobe(CC1101_SFTX)));
-
-	LOG_ERRB(
-	kputs("[");
+	LOG_INFOB( kputs("TX[");
 	for (size_t i = 0; i < size; i++)
 		kprintf("%x ", data[i]);
 	kprintf("] %d\n", size);
@@ -259,61 +206,52 @@ static size_t radio_recv(struct KFile *_fd, void *buf, size_t size)
 	uint8_t *data = (uint8_t *)buf;
 	Radio *fd = RADIO_CAST(_fd);
 
-	if (!radio_waitIdle(1000))
-	{
-		fd->error = RADIO_RX_ERR;
-		return RADIO_RX_ERR;
-	}
-
-	// Flush Rx FiFo
+	radio_waitIdle(CC1101_GOIDLE_TIMEOUT);
 	cc1101_strobe(CC1101_SFRX);
-	// Go in Rx mode
 	cc1101_strobe(CC1101_SRX);
 
 	// Waiting data from air..
 	if (!wait_fifoAvail(fd->recv_timeout))
 	{
-		// Flush Rx FiFo
-		cc1101_strobe(CC1101_SFRX);
-		fd->error = RADIO_RX_TIMEOUT;
-
-		LOG_ERR("Rx timeout..\n");
-
 		radio_goIdle();
+		cc1101_strobe(CC1101_SFRX);
+		LOG_INFO("Rx timeout..\n");
+		fd->error = RADIO_RX_TIMEOUT;
 		return RADIO_RX_TIMEOUT;
 	}
 
-	kprintf("RxLen[%d]\n", size);
-	fd->status = cc1101_read(CC1101_RXFIFO, data, size);
-	size_t rx_len = MIN((size_t)data[0], size);
+	uint8_t rx_data;
+	cc1101_read(CC1101_RXFIFO, &rx_data, 1);
 
-	cc1101_read(CC1101_LQI, &fd->lqi, 1);
-	cc1101_read(CC1101_RSSI, &fd->rssi, 1);
+	size_t rx_len = MIN((size_t)rx_data, size);
+	LOG_INFO("RecvLen[%d] [%d] s[%d]\n", rx_len, size, \
+			STATUS_STATE(cc1101_read(CC1101_RXFIFO, data, rx_len)));
+
+	radio_waitIdle(CC1101_GOIDLE_TIMEOUT);
+	cc1101_read(CC1101_LQI, &fd->lqi, sizeof(uint8_t));
+	cc1101_read(CC1101_RSSI, &fd->rssi, sizeof(uint8_t));
 	fd->rssi = cc1101_rssidBm(fd->rssi, 74);
-	LOG_INFO("Rx:s1[%d]lqi[%d]\n", STATUS_STATE(fd->status),
-						fd->lqi);
+	fd->status = cc1101_strobe(CC1101_SFRX);
 
-	LOG_ERRB(
-	kputs("[");
+	LOG_INFO("Rx: s1[%d], lqi[%d] rssi[%d]\n", STATUS_STATE(fd->status),
+						fd->lqi & ~CC1101_LQI_CRC_OK, fd->rssi);
+
+	LOG_INFOB(kputs("RX[");
 	for (size_t i = 0; i < rx_len; i++)
 		kprintf("%x ", data[i]);
 	kprintf("] %d\n", rx_len);
 	);
 
-	// No more data, flush rx fifo.
-	cc1101_strobe(CC1101_SFRX);
-	radio_goIdle();
-
 	/* check if fifo data is valid packet */
-	if (!(fd->lqi & CC1101_LQI_CRC_OK))
+	if (fd->lqi & CC1101_LQI_CRC_OK)
 	{
-		fd->lqi = 0;
-		fd->error = RADIO_RX_ERR;
-		return RADIO_RX_ERR;
+		fd->lqi &= ~CC1101_LQI_CRC_OK;
+		return rx_len;
 	}
 
-	fd->lqi &= ~CC1101_LQI_CRC_OK;
-	return rx_len;
+	fd->lqi = 0;
+	fd->error = RADIO_RX_ERR;
+	return RADIO_RX_ERR;
 }
 
 void radio_init(Radio *fd, const Setting * settings)
@@ -333,4 +271,3 @@ void radio_init(Radio *fd, const Setting * settings)
 	fd->fd.clearerr = radio_clearerr;
 	fd->error = 0;
 }
-
